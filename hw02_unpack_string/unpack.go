70ba7e96968d4bfa3fcec2cf986aed72da850156
Konstantin Yakimenko <jakimenko.k@gmail.com>
2021-11-27 13:28:57 +0300

    hw 02

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,40 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(input string) (string, error) {
+	reverseStr := reverse(input)
+	countSymbol := 1
+	isPrevDigit := false
+	var b strings.Builder
+	for _, sym := range reverseStr {
+		if unicode.IsDigit(sym) {
+			if isPrevDigit {
+				return "", ErrInvalidString
+			}
+			countSymbol = int(sym - '0')
+			isPrevDigit = true
+		} else {
+			b.WriteString(strings.Repeat(string(sym), countSymbol))
+			countSymbol = 1
+			isPrevDigit = false
+		}
+	}
+	if isPrevDigit {
+		return "", ErrInvalidString
+	}
+	return reverse(b.String()), nil
+}
+
+func reverse(input string) string {
+	r := []rune(input)
+	for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 {
+		r[i], r[j] = r[j], r[i]
+	}
+	return string(r)
 }
